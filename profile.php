93f6b6a0db7d69028ba33d33cec6174a644a0760
branikita <nikita-bragin@mail.ru>
2014-12-25 22:29:42 +0300

    profile page

---
+++ profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<? include('blocks/head.php'); ?>
+<body>
+<div
+    style="background: url('images/stuff/your-profile.jpg'); width: 640px; height: 1136px; position: absolute; z-index: 100; top: -100px; left: 0; opacity: 0.5;display: none;"></div>
+<div class="content js-content">
+    <? include('blocks/header.php'); ?>
+
+    <main class="white-screen profile-page">
+        <div class="ribbon">
+            <div class="ribbon__content">YOUR PROFILE</div>
+        </div>
+        <section class="white-screen__content">
+            <figure class="user-avatar">
+                    <span>
+                        <img src="images/avatars/coach.jpg" width="170" height="170" alt="Team photo">
+                    </span>
+                <figcaption>
+                    <h2>Andrew Welstead</h2>
+
+                    <h3>TEAM WELLY</h3>
+                </figcaption>
+            </figure>
+            <a href="#" class="button button-black profile-page__dressing-room">
+                BACK TO DRESSING ROOM
+                <span class="sprite icon icon-go"></span>
+            </a>
+        </section>
+    </main>
+</div>
+</body>
+</html>
