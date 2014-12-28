286a0f663da4e61dcca438f400a8c0a2a1e7226e
branikita <nikita-bragin@mail.ru>
2014-12-28 17:17:08 +0300

    messages

---
+++ messages.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<?
+$title = 'MESSAGES';
+include('blocks/head.php');
+?>
+<body>
+<div
+    style="background: url('images/stuff/messages.jpg'); width: 640px; height: 1136px; position: absolute; z-index: 100; top: -100px; left: 0; opacity: 0.5;display: none;"></div>
+<div class="content bg-dressing-room js-content">
+    <? include('blocks/dressing_room_header.php'); ?>
+
+    <main class="messages-page">
+            <section class="messages-list">
+                <figure class="clearfix messages-list__item">
+                    <div class="messages-list__item__icon">
+                        <span class="sprite icon icon-big-message"></span>
+                    </div>
+                    <figcaption>
+                        <h3>Well done mate, keep up the great work!</h3>
+                        <span>JOHN SMITH</span>
+                    </figcaption>
+                </figure>
+                <figure class="clearfix messages-list__item">
+                    <div class="messages-list__item__icon">
+                        <span class="sprite icon icon-big-message"></span>
+                    </div>
+                    <figcaption>
+                        <h3>Well done mate, keep up the great work!</h3>
+                        <span>JOHN SMITH</span>
+                    </figcaption>
+                </figure>
+                <figure class="clearfix messages-list__item">
+                    <div class="messages-list__item__icon">
+                        <span class="sprite icon icon-big-message"></span>
+                    </div>
+                    <figcaption>
+                        <h3>Well done mate, keep up the great work!</h3>
+                        <span>JOHN SMITH</span>
+                    </figcaption>
+                </figure>
+                <figure class="clearfix messages-list__item">
+                    <div class="messages-list__item__icon">
+                        <span class="sprite icon icon-big-message"></span>
+                    </div>
+                    <figcaption>
+                        <h3>Well done mate, keep up the great work!</h3>
+                        <span>JOHN SMITH</span>
+                    </figcaption>
+                </figure>
+                <figure class="clearfix messages-list__item">
+                    <div class="messages-list__item__icon">
+                        <span class="sprite icon icon-big-message"></span>
+                    </div>
+                    <figcaption>
+                        <h3>Well done mate, keep up the great work!</h3>
+                        <span>JOHN SMITH</span>
+                    </figcaption>
+                </figure>
+            </section>
+            <div class="go-back">
+                <a href="/create_profile.php"><span class="sprite icon icon-back"></span>Tap here to go back</a>
+            </div>
+    </main>
+
+    <? include('blocks/footer.php'); ?>
+</div>
+</body>
+</html>
